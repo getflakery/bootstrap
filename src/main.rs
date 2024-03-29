1a79ae77f8da4116d87a5ea110a5a011feca7515
r33drichards <rwendt1337@gmail.com>
2024-03-28 21:04:01 -0700

    get files

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 
+// file struct
+struct File {
+    key: String,
+    value: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let res = reqwest::get("http://169.254.169.254/latest/meta-data/tags/instance").await?;
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
-
     let body = res.text().await?;
-    println!("Body:\n{}", body);
+
+    let fileKeys = body.split("\n").filter(
+        |key| key.starts_with("file:")
+    ).collect::<Vec<&str>>();
+
+    let files = fileKeys.iter().map(|key| {
+        let res = reqwest::get(
+            format!("http://169.254.169.254/latest/meta-data/tags/instance/{}", key)
+        ).await?;
+        let value = res.text().await?;
+        File {
+            key: key.to_string().strip_prefix("file:").unwrap_or(&key).to_string(),
+            value: value,
+        }
+    }).collect::<Vec<File>>();
+
+    for file in files {
+        println!("{}: {}", file.key, file.value);
+    }
+
+
+
+
+
     Ok(())
 }
