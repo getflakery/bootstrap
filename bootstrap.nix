e0bfd9f99096048df435781cb504d6737dd075b1
r33drichards <rwendt1337@gmail.com>
2024-03-29 11:48:33 -0700

    add garnix binary cache

--- bootstrap.nix
+++ bootstrap.nix
@@ -4,6 +4,20 @@ app:
 
   system.stateVersion = "23.05";
 
+    nix = {
+    settings = {
+      substituters = [
+        "https://cache.garnix.io"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
+    };
+  };
+
   systemd.services.bootstrap = {
     description = "bootstraper";
     after = [ "network.target" ];
