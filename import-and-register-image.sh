332736fbabd923d5d031a1f248c6a2fbebcb3207
r33drichards <rwendt1337@gmail.com>
2024-04-01 23:43:11 -0700

    blursed

--- import-and-register-image.sh
+++ import-and-register-image.sh
@@ -2,5 +2,6 @@
 echo 'import id: ' $1
 echo 'waiting for image to be imported'
 aws ec2 wait snapshot-imported --import-task-ids $1 --region us-west-1 
+
 echo 'registering image' 
-aws ec2 register-image --name "NixOS-$(date +%Y%m%d)" --architecture x86_64 --root-device-name /dev/sda1 --block-device-mappings "DeviceName=/dev/sda1,Ebs=$1" --virtualization-type hvm --region us-west-1
+aws ec2 register-image --name "NixOS-$(date +%Y%m%d)" --architecture x86_64 --root-device-name /dev/sda1 --block-device-mappings "DeviceName=/dev/sda1,Ebs=`aws ec2 describe-import-snapshot-tasks --import-task-ids import-snap-025b351f808c91516  --no-cli-pager | jq '.ImportSnapshotTasks[0].SnapshotTaskDetail.SnapshotId' | sed 's/"//g' | sed 's/ +//g'`" --virtualization-type hvm --region us-west-1
