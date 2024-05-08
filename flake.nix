c18cfba6fa468d577278cb73d5bfb63242df6c47
rw <rw@jjk.is>
2024-05-08 14:18:36 -0700

    fw r

--- flake.nix
+++ flake.nix
@@ -192,6 +192,15 @@
 
               nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+              networking.firewall = {
+                enable = true;
+                allowedTCPPorts = [ 8000 ];
+                # allowedUDPPortRanges = [
+                #   { from = 4000; to = 4007; }
+                #   { from = 8000; to = 8010; }
+                # ];
+              };
+
             }
           ];
         };
