fd5ced7b1b531a46dd16e61306d2504c63a3cbd3
rw <rw@jjk.is>
2024-05-08 13:51:03 -0700

    add connectivity

--- flake.nix
+++ flake.nix
@@ -149,6 +149,31 @@
                 self.nixosModules."${system}".webserver
               ];
               services.webserver.enable = true;
+              services.tailscale = {
+                enable = true;
+                authKeyFile = "/tsauthkey";
+                extraUpFlags = [ "--ssh" ];
+              };
+
+              users.users.flakery = {
+                isNormalUser = true;
+                extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+                password = "flakery"; # Set the password for the user.
+              };
+              # allow sudo without password for wheel
+              security.sudo.wheelNeedsPassword = false;
+
+              services.openssh = {
+                enable = true;
+                # require public key authentication for better security
+                settings.PasswordAuthentication = false;
+                settings.KbdInteractiveAuthentication = false;
+              };
+
+              users.users."flakery".openssh.authorizedKeys.keys = [
+                # replace with your ssh key 
+                "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCs/e5M8zDNH5DUmqCGKM0OhHKU5iHFum3IUekq8Fqvegur7G2fhsmQnp09Mjc5pEw2AbfTYz11WMHsvC5WQdRWSS2YyZHYsPb9zIsVBNcss+H5x63ItsDjmbrS6m/9r7mRBOiN265+Mszc5lchFtRFetpi9f+EBis9r8atyPlsz86IoS2UxSSWonBARU4uwy2+TT7+mYg3cQf7kp1Y1sTqshXmcHUC5UVSRk3Ny9IbIMhk19fOxr3y8gaXoT5lB0NSLO8XFNbNT6rjZXH1kpiPJh3xLlWBPQtbcLrpm8oSS51zH7+zAGb7mauDHu2RcfBgq6m1clZ6vff65oVuHOI7"
+              ];
             }
           ];
         };
