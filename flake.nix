9f8442d9758701e2eb6d92c299398a1b7abbccbb
rw <rw@jjk.is>
2024-05-08 13:56:02 -0700

    subs

--- flake.nix
+++ flake.nix
@@ -174,6 +174,24 @@
                 # replace with your ssh key 
                 "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCs/e5M8zDNH5DUmqCGKM0OhHKU5iHFum3IUekq8Fqvegur7G2fhsmQnp09Mjc5pEw2AbfTYz11WMHsvC5WQdRWSS2YyZHYsPb9zIsVBNcss+H5x63ItsDjmbrS6m/9r7mRBOiN265+Mszc5lchFtRFetpi9f+EBis9r8atyPlsz86IoS2UxSSWonBARU4uwy2+TT7+mYg3cQf7kp1Y1sTqshXmcHUC5UVSRk3Ny9IbIMhk19fOxr3y8gaXoT5lB0NSLO8XFNbNT6rjZXH1kpiPJh3xLlWBPQtbcLrpm8oSS51zH7+zAGb7mauDHu2RcfBgq6m1clZ6vff65oVuHOI7"
               ];
+
+              nix = {
+                settings = {
+                  substituters = [
+                    "https://cache.garnix.io"
+                    "https://nix-community.cachix.org"
+                    "https://cache.nixos.org/"
+                  ];
+                  trusted-public-keys = [
+                    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+                  ];
+                };
+              };
+
+
+              nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
             }
           ];
         };
