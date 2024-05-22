1a219222d3137bcdbb8d6c32f1381cf3d44e62b3
rw <rw@jjk.is>
2024-05-21 23:27:28 -0700

    enable lb

--- serve.py
+++ serve.py
@@ -9,6 +9,13 @@ port = 8080
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
+        import argparse
+
+        args = argparse.ArgumentParser(description="A script to serve the necessary files for the flake builder.")
+        # args.add_argument("--enable-lb", help="add --lb bootstrap arg", default="false")
+        # boolean
+        args.add_argument("--enable-lb", help="add --lb bootstrap arg", action="store_true")
+        args = args.parse_args()
         if self.path == "/turso_token":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
@@ -29,6 +36,23 @@ class MyServer(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"/nix/store/yaak6mprs2w5b2vrf5dzq2lwd76mpry0-test-flake#hello-flakery")
+        # deployment_id
+        elif self.path == "/deployment_id":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"00f00f")
+        # bootstrap_args
+        elif self.path == "/bootstrap_args" and args.enable_lb:
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"--lb")
+        elif self.path == "/bootstrap_args":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"")
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
@@ -36,6 +60,7 @@ class MyServer(BaseHTTPRequestHandler):
             self.wfile.write(b"Path not found")
 
 if __name__ == "__main__":
+
     webServer = HTTPServer((hostname, port), MyServer)
     print(f"Server started http://{hostname}:{port}")
 
