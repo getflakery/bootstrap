39ade8deca8f6b44efe2d9ddcfabc867e8483b12
rw <rw@jjk.is>
2024-05-21 23:59:35 -0700

    impure

--- service.app.nix
+++ service.app.nix
@@ -7,7 +7,7 @@ let
       export DEPLOYMENT=`${app}/bin/app --print-deployment-id`
       export NIX_CONFIG="access-tokens = github.com=`${app}/bin/app --print-github-token`"
       ${pkgs.fluent-bit}/bin/fluent-bit \
-        -i exec -p 'command=${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake `${app}/bin/app --print-flake` --refresh --no-write-lock-file 2>&1' \
+        -i exec -p 'command=${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake `${app}/bin/app --print-flake` --refresh --no-write-lock-file --impure 2>&1' \
         -o http://flakery.dev/api/deployments/log/rebuild/$DEPLOYMENT -p 'tls=on' -m '*' -p 'Port=443' -p 'Format=json'
   '');
 in
