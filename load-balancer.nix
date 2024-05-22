7352e16ebd1ebb3f15ede33338d381e377b13c47
rw <rw@jjk.is>
2024-05-22 18:48:59 -0500

    var lib

--- load-balancer.nix
+++ load-balancer.nix
@@ -16,18 +16,6 @@ in
     # Path to the Traefik binary
     package = pkgs.traefik;
 
-    extraConfigFiles = {
-      "/var/lib/traefik/acme.json" = {
-        text = '';
-          # Empty JSON object to initialize the file
-          {}
-        '';
-        permissions = "0600";
-        owner = "traefik";
-        group = "traefik";
-      };
-    };
-
     staticConfigOptions = {
       entryPoints = {
         web = {
