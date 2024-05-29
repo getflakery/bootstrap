669fca2f5c424fa6bf2f9005997fb7a6db237130
rw <rw@jjk.is>
2024-05-29 09:16:33 -0500

    get public ip address

--- bootstrap/src/lb.rs
+++ bootstrap/src/lb.rs
@@ -2,8 +2,29 @@
 use anyhow::Result;
 use crate::EC2TagData;
 use crate::File;
+use reqwest::get;
 
-pub fn bootstrap_load_balancer(
+
+async fn get_ip_address() -> Result<String> {
+    let response = get("http://169.254.169.254/latest/meta-data/public-ipv4").await?;
+    let ip_address = response.text().await?;
+    Ok(ip_address)
+}
+
+async fn try_get_ip_address() -> Result<String> {
+    for _ in 0..100 {
+        match get_ip_address().await {
+            Ok(ip_address) => return Ok(ip_address),
+            Err(e) => {
+                eprintln!("{:?} {}:{}", e, file!(), line!());
+                std::thread::sleep(std::time::Duration::from_secs(1));
+            }
+        }
+    }
+    Err(anyhow::anyhow!("could not get ip address"))
+}
+
+pub async fn bootstrap_load_balancer(
     ec2_tag_data: &EC2TagData,
 ) -> Result<()> {
     println!("bootstrap_load_balancer");
@@ -18,6 +39,8 @@ pub fn bootstrap_load_balancer(
     file.write()?;
     println!("wrote /etc/deployment_id");
 
-
+    // try to get the public ip address of the instance
+    let ip = try_get_ip_address().await?;
+    println!("public ip address: {}", ip);
     Ok(())
 }
