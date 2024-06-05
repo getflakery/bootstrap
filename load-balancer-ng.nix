50e8652ddf77e97060eb689822ea75e0827c4be0
rw <rw@jjk.is>
2024-06-05 10:12:32 -0700

    load balancer ng

---
+++ load-balancer-ng.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  # Enable the Traefik service
+  services.traefik = {
+    enable = true;
+
+    # Path to the Traefik binary
+    package = pkgs.traefik;
+
+    staticConfigOptions = {
+      entryPoints = {
+        web = {
+          address = ":80";
+        };
+        websecure = {
+          address = ":443";
+        };
+      };
+      certificatesResolvers = {
+        letsencrypt = {
+          acme = {
+            email = "rwendt1337@gmail.com";
+            storage = "/var/lib/traefik/acme.json";
+            httpChallenge = {
+              entryPoint = "web";
+            };
+          };
+        };
+      };
+      providers = {
+        http = {
+          endpoint = "https://flakery.dev/api/deployments/lb-config-ng";
+          pollInterval = "10s";
+        };
+      };
+    };
+  };
+}
