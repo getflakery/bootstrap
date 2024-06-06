9a04fcbe53b79172b60fc0076c8154db7edb8689
rw <rw@jjk.is>
2024-06-06 12:09:29 -0700

    try to issue certs

--- load-balancer-ng.nix
+++ load-balancer-ng.nix
@@ -28,11 +28,39 @@
     ];
   };
 
-  services.certbot = {
+  security.acme.acceptTerms = true;
+  security.acme.defaults.email = "rwendt1337@gmail.com";
+
+  # /var/lib/acme/.challenges must be writable by the ACME user
+  # and readable by the Nginx user. The easiest way to achieve
+  # this is to add the Nginx user to the ACME group.
+  users.users.nginx.extraGroups = [ "acme" ];
+
+  services.nginx = {
     enable = true;
+    virtualHosts = {
+      "acmechallenge.flakery.xyz" = {
+        # Catchall vhost, will redirect users to HTTPS for all vhosts
+        serverAliases = [ "*.flakery.xyz" ];
+        locations."/.well-known/acme-challenge" = {
+          root = "/var/lib/acme/.challenges";
+        };
+        locations."/" = {
+          return = "301 https://$host$request_uri";
+        };
+      };
+    };
+  };
+
+  security.acme.certs."lb.flakery.xyz" = {
+    webroot = "/var/lib/acme/.challenges";
     email = "rwendt1337@gmail.com";
-    acceptTerms = true;
-    domains = [ "lb.flakery.xyz" ];
+    # Ensure that the web server you use can read the generated certs
+    # Take a look at the group option for the web server you choose.
+    group = "nginx";
+    # Since we have a wildcard vhost to handle port 80,
+    # we can generate certs for anything!
+    # Just make sure your DNS resolves them.
   };
 
   # Enable the Traefik service
@@ -62,6 +90,7 @@
           };
         };
       };
+      # route lb.flakery.dev to 
       providers = {
         http = {
           endpoint = "https://flakery.dev/api/deployments/lb-config-ng";
