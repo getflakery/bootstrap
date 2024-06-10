60ca8d57d0f4e2a5eb8065e68ed93972558117fd
rw <rw@jjk.is>
2024-06-10 12:13:30 -0700

    shell app

--- flake.nix
+++ flake.nix
@@ -149,12 +149,12 @@
           exit 0
         '';
 
-        vec0 = pkgs.writeScript "vec0.sh" ''
+        vec0 = pkgs.writeShellApplication "vec0.sh" ''
           ${exit0} | ${stdoutScript}
           echo $?
         '';
 
-        vec1 = pkgs.writeScript "vec1.sh" ''
+        vec1 = pkgs.writeShellApplication "vec1.sh" ''
           ${exit1} | ${stdoutScript}
           echo $?
         '';
