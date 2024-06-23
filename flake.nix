2e484503777db985ad1006f25794c34a14746fa6
rw <rw@jjk.is>
2024-06-22 17:14:24 -0700

    try reading from file

--- flake.nix
+++ flake.nix
@@ -471,22 +471,7 @@
             };
 
             interactive.nodes.machine1 = import ./debug-host-module.nix;
-
-            testScript = ''
-              machine.start()
-              # assert /foo/bar.txt contains secret 
-              machine1.wait_for_file("/foo/bar.txt")
-              response = machine1.succeed("cat /foo/bar.txt")
-              assert "secret" in response
-              # todo add me back
-              response = machine1.succeed("sqlite3 /tmp/db.sqlite3 'SELECT * FROM target;'")
-              print(response)
-              assert "00f00f" in response
-              # select production from deployment
-              response = machine1.succeed("sqlite3 /tmp/db.sqlite3 'SELECT production FROM deployments WHERE id == ''00f00f'';'")
-              print(response)
-              assert "1" == response
-            '';
+            testScript = builtins.readFile ./testScript.py
           };
 
 
