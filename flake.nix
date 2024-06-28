256371c3d7fae5a860a760e82e1459e4c627ca01
rw <rw@jjk.is>
2024-06-28 14:03:27 -0700

    add self as admin

--- flake.nix
+++ flake.nix
@@ -348,6 +348,7 @@
                   WOODPECKER_GITHUB_CLIENT = "Ov23li77VshZc9W7M4Gp";
                   WOODPECKER_GITHUB_SECRET = builtins.readFile /github-client-secret;
                   WOODPECKER_AGENT_SECRET = builtins.readFile /agent-secret;
+                  WOODPECKER_ADMIN="r33drichards";
 
                 };
                 # You can pass a file with env vars to the system it could look like:
