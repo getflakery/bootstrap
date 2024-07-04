d8001b21846037535ebd3f4111addf36dec0310f
rw <rw@jjk.is>
2024-07-03 18:46:16 -0700

    loki

--- flake.nix
+++ flake.nix
@@ -379,103 +379,31 @@
                 #
                 services.loki = {
                   enable = true;
-                  configuration = {
-                    server.http_listen_port = 3030;
-                    auth_enabled = false;
-
-                    ingester = {
-                      lifecycler = {
-                        address = "127.0.0.1";
-                        ring = {
-                          kvstore = {
-                            store = "inmemory";
-                          };
-                          replication_factor = 1;
-                        };
-                      };
-                      chunk_idle_period = "1h";
-                      max_chunk_age = "1h";
-                      chunk_target_size = 999999;
-                      chunk_retain_period = "30s";
-                    };
-
-                    schema_config = {
-                      configs = [{
-                        from = "2022-06-06";
-                        store = "boltdb-shipper";
-                        object_store = "filesystem";
-                        schema = "v11";
-                        index = {
-                          prefix = "index_";
-                          period = "24h";
-                        };
-                      }];
-                    };
-
-                    storage_config = {
-                      boltdb_shipper = {
-                        active_index_directory = "/var/lib/loki/boltdb-shipper-active";
-                        cache_location = "/var/lib/loki/boltdb-shipper-cache";
-                        cache_ttl = "24h";
-                      };
-
-                      filesystem = {
-                        directory = "/var/lib/loki/chunks";
-                      };
-                    };
-
-                    limits_config = {
-                      reject_old_samples = true;
-                      reject_old_samples_max_age = "168h";
-                    };
-
-     
-
-                    table_manager = {
-                      retention_deletes_enabled = false;
-                      retention_period = "0s";
-                    };
-
-                    compactor = {
-                      working_directory = "/var/lib/loki";
-                      compactor_ring = {
-                        kvstore = {
-                          store = "inmemory";
-                        };
-                      };
-                    };
-                  };
-                  # user, group, dataDir, extraFlags, (configFile)
+                  configFile = "${pkgs.grafana-loki.src}/cmd/loki/loki-local-config.yaml";
                 };
                 services.promtail = {
                   enable = true;
                   configuration = {
                     server = {
-                      http_listen_port = 3031;
+                      http_listen_port = 9080;
                       grpc_listen_port = 0;
                     };
-                    positions = {
-                      filename = "/tmp/positions.yaml";
-                    };
-                    clients = [{
-                      url = "http://127.0.0.1:3030/loki/api/v1/push";
-                    }];
-                    scrape_configs = [{
-                      job_name = "journal";
-                      journal = {
-                        max_age = "12h";
-                        labels = {
-                          job = "systemd-journal";
-                          host = "pihole";
-                        };
-                      };
-                      relabel_configs = [{
-                        source_labels = [ "__journal__systemd_unit" ];
-                        target_label = "unit";
-                      }];
-                    }];
+                    clients = [{ url = "http://localhost:3100/loki/api/v1/push"; }];
+                    scrape_configs = [
+                      {
+                        job_name = "system";
+                        static_configs = [
+                          {
+                            targets = [ "localhost" ];
+                            labels = {
+                              job = "varlogs";
+                              __path__ = "/var/log/*log";
+                            };
+                          }
+                        ];
+                      }
+                    ];
                   };
-                  # extraFlags
                 };
               }
             ];
