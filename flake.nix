5c7a3e41c17943d09a811db393ba32efd31c7b2f
rw <rw@jjk.is>
2024-07-09 20:51:12 -0700

    add debug

--- flake.nix
+++ flake.nix
@@ -562,7 +562,7 @@
             flakery.nixosConfigurations.base
             sshconfMod
             {
-              networking.firewall.allowedTCPPorts = [ 5000 ];
+              networking.firewall.allowedTCPPorts = [ 5000 9002 ];
               # set perms fpr "/var/cache-priv-key.pem" to 600 
               # before running nix-serve
               systemd.services.setPerms = {
@@ -587,6 +587,60 @@
 
               # add flakery as trusted user
               nix.settings.trusted-users = [ "flakery" ];
+              services.promtail = {
+                enable = true;
+                configuration = {
+                  server = {
+                    http_listen_port = 9080;
+                    grpc_listen_port = 0;
+                  };
+                  clients = [{ url = "http://grafana:3100/loki/api/v1/push"; }];
+                  scrape_configs = [
+                    {
+                      job_name = "system";
+                      static_configs = [
+                        {
+                          targets = [ "localhost" ];
+                          labels = {
+                            job = "varlogs";
+                            __path__ = "/var/log/*log";
+                          };
+                        }
+
+                      ];
+                    }
+                    {
+                      job_name = "journal";
+                      journal = {
+                        max_age = "12h";
+                        labels = {
+                          job = "systemd-journal";
+                          host = "woodpecker";
+                        };
+                      };
+                      relabel_configs = [{
+                        source_labels = [ "__journal__systemd_unit" ];
+                        target_label = "unit";
+                      }];
+                    }
+
+                  ];
+                };
+              };
+
+              services.prometheus = {
+                enable = true;
+                port = 9090;
+                exporters = {
+                  node = {
+                    enable = true;
+                    enabledCollectors = [ "systemd" ];
+                    port = 9002;
+                  };
+
+                };
+              };
+
             }
           ];
         };
